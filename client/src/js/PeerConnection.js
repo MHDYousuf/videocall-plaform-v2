7af3d4d1c1840091b2eb5c387d4876c0b54a2c02
MHDYousuf <mohd4yousuf@gmail.com>
2020-06-14 19:17:58 +0530

    changed turn server

--- client/src/js/PeerConnection.js
+++ client/src/js/PeerConnection.js
@@ -14,9 +14,9 @@ const PC_CONFIG = {
       urls: "stun:stun.l.google.com:19302",
     },
     {
-      urls: "turn:192.158.29.39:3478?transport=udp",
-      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      username: "28224511:1379330808",
+      urls: "turn:numb.viagenie.ca",
+      credential: "Luffy123",
+      username: "mohd4yousuf@gmail.com",
     },
     {
       urls: "turn:192.158.29.39:3478?transport=tcp",

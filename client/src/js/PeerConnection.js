bac13c56f090afeeced89ef75fa7cace89ffc0da
MHDYousuf <mohd4yousuf@gmail.com>
2020-06-14 18:22:43 +0530

    IceServers

--- client/src/js/PeerConnection.js
+++ client/src/js/PeerConnection.js
@@ -8,7 +8,27 @@ import MediaDevice from "./MediaDevice";
 import Emitter from "./Emitter";
 import socket from "./socket";
 
-const PC_CONFIG = { iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }] };
+const PC_CONFIG = {
+  iceServers: [
+    {
+      urls: [
+        {
+          url: "stun:stun.l.google.com:19302",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=udp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+        {
+          url: "turn:192.158.29.39:3478?transport=tcp",
+          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+          username: "28224511:1379330808",
+        },
+      ],
+    },
+  ],
+};
 
 class PeerConnection extends Emitter {
   /**

5aa15df6c43cbc12e8a9d011d87bfa430f8cdabf
MHDYousuf <mohd4yousuf@gmail.com>
2020-06-14 18:38:31 +0530

    iceServer syntax (not sure)

--- client/src/js/PeerConnection.js
+++ client/src/js/PeerConnection.js
@@ -11,21 +11,17 @@ import socket from "./socket";
 const PC_CONFIG = {
   iceServers: [
     {
-      urls: [
-        {
-          url: "stun:stun.l.google.com:19302",
-        },
-        {
-          url: "turn:192.158.29.39:3478?transport=udp",
-          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-          username: "28224511:1379330808",
-        },
-        {
-          url: "turn:192.158.29.39:3478?transport=tcp",
-          credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-          username: "28224511:1379330808",
-        },
-      ],
+      urls: "stun:stun.l.google.com:19302",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=udp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
+    },
+    {
+      urls: "turn:192.158.29.39:3478?transport=tcp",
+      credential: "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+      username: "28224511:1379330808",
     },
   ],
 };
